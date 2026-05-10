ce46a02b7677fc96942acc9875dbe99df3d598a0
Mohamed Adel <87273126+M0hamedAdel@users.noreply.github.com>
2026-05-10 10:36:15 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+model = pickle.load(open('breast_cancer_model.pkl', 'rb'))
+
+st.title("Breast Cancer Classification")
+
+radius_mean = st.number_input("Radius Mean")
+texture_mean = st.number_input("Texture Mean")
+perimeter_mean = st.number_input("Perimeter Mean")
+area_mean = st.number_input("Area Mean")
+smoothness_mean = st.number_input("Smoothness Mean")
+
+features = np.array([[
+
+    radius_mean,
+    texture_mean,
+    perimeter_mean,
+    area_mean,
+    smoothness_mean,
+
+    0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0,
+    0
+
+]])
+
+if st.button("Predict"):
+
+    prediction = model.predict(features)
+
+    if prediction[0] == 1:
+        st.error("Malignant Tumor Detected")
+    else:
+        st.success("Benign Tumor")
